2c0265ddf52436732d628bf117853b6dc56ee1e1
mzzsml <mzzsml@protonmail.ch>
2019-08-04 11:28:11 +0200

    add audio display

--- scripts/dwm_status.sh
+++ scripts/dwm_status.sh
@@ -1,11 +1,27 @@
 #!/bin/zsh
 
-function clock() {
+function get_clock() {
 	$clock=date "+%H:%M"
 }
 
-function date() {
+function get_date() {
 	$date=date "+%d/%m/%Y"
 }
 
-xsetroot -name "[$(date)] [$(clock)]"
+function get_volume() {
+	vol="$(amixer get Master | tail -n 1 | awk '{print $6}')"
+
+	if [ $vol = "[off]" ]
+	then
+		echo "[Mute]"
+	else
+		echo "$(amixer get Master | tail -n 1 | awk '{print $5}')"
+	fi
+}
+
+while true
+do
+
+	xsetroot -name "$(get_volume) [$(get_date)] [$(get_clock)]"
+
+done
